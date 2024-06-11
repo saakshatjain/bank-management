97bfeabd3b8cf56137e411ec6b83f4a3da2c35e6
saakshatjain <123252511+saakshatjain@users.noreply.github.com>
2024-06-11 12:48:54 +0530

    Add files via upload
    Added routes

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const cors = require("cors");
-const rootRouter = require("./routes/index");
+const userRouter = require("./user");
+const accountRouter = require("./account");
 
-const app = express();
+const router = express.Router();
 
-app.use(cors());
-app.use(express.json());
-app.get("/" , function(req,res) {
-    res.send("hhello world");
-})
-app.use("/api/v1", rootRouter);
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
-app.listen(3000);
+module.exports = router;
