e29712b6caee0a282674da7f600630e714941e5b
saakshatjain <123252511+saakshatjain@users.noreply.github.com>
2024-06-13 14:59:57 +0530

    Added stlying in sign up

--- frontend/paytm-frontend/src/components/signup.jsx
+++ frontend/paytm-frontend/src/components/signup.jsx
@@ -42,21 +42,30 @@ export default function Signup() {
 
 
     }
-    return <div>
-    First Name<input type="text" onChange={(e)=> setname(e.target.value)}></input>
+    return <div className="bg-blue-200">
+       <div className="max-w-md mx-auto p-8 bg-gray-100 shadow-md rounded-lg">
+        <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+        <label className="block text-gray-700">First Name</label>
+    <input type="text" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e)=> setname(e.target.value)}></input>
     <br></br>
-    Last name<input type="text" onChange={(e)=> setlastname(e.target.value)}></input>
+    <label className="block text-gray-700">Last Name</label>
+  <input type="text" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e)=> setlastname(e.target.value)}></input>
     <br></br>
-    Username<input type="text" onChange={(e)=> setusername(e.target.value)}></input>
+    <label className="block text-gray-700">Username</label>
+ <input type="text" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e)=> setusername(e.target.value)}></input>
     <br></br>
-    Password <input type="text" onChange={(e)=> setpassword(e.target.value)}></input>
+    <label className="block text-gray-700">Password</label>
+     <input type="password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e)=> setpassword(e.target.value)}></input>
     <br></br>
-    <button onClick={authorize}>Sign up</button>
     <br></br>
-    Already have an account?
-    <button onClick={movetosignin}>Sign In</button>
+    <button onClick={authorize} className="w-full py-2 mb-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors">Sign up</button>
+    <br></br>
+    <p className="mb-4 text-center">Already have an account?</p>
+    <div className="text-center">
+    <button onClick={movetosignin} className="text-center py-2 px-4 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition-colors">Sign In</button>
      <br></br>
-    <h4>{error}</h4>
-
+     </div>
+    <h4 className="mt-4 text-red-500 text-center">{error}</h4>
+    </div>
     </div>
 }
