0222e7f0159a116a0b0aee03738198dc6e39cc5f
saakshatjain <123252511+saakshatjain@users.noreply.github.com>
2024-06-13 16:21:07 +0530

    Added functionalties

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -97,7 +97,7 @@ const updateBody = zod.object({
     lastname: zod.string().optional(),
 })
 
-router.put("/", authMiddleware, async (req, res) => {
+router.put("/update", authMiddleware, async (req, res) => {
     const { success } = updateBody.safeParse(req.body)
     if (!success) {
         res.status(411).json({
